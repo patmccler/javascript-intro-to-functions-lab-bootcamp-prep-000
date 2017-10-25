ffd1874894f6bcbd8b1ddb45b210815f10355438
Patrick McClernan <patmccler@gmail.com>
2017-10-25 00:06:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShourt(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+functoin sayHiToGrandma(string) {
+  if (string === whisper(string)) {
+    return "I can't hear you!"
+  }
+  if (string === shout(string)) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.")
+    return "I love you, too."
+}
